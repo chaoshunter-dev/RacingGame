808d7eff4b8f5854850804634c475b96fbfa804c
chaoshunter-dev <58077034+chaoshunter-dev@users.noreply.github.com>
2021-12-15 17:39:08 +0000

    particles and C++

--- KIA_Game_Project/Intermediate/Build/Win64/KIA_Game_ProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30137
-#include "C:/Users/keiro/Documents/GitHub/RacingGame/KIA_Game_Project/Intermediate/Build/Win64/KIA_Game_ProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- KIA_Game_Project/Source/KIA_Game_Project/KIA_Game_Project.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "KIA_Game_Project.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, KIA_Game_Project, "KIA_Game_Project" );
